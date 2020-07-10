0eb62e67a7d159785bc5120807d5add58574d291
rosejtec@gmail.com <rosej.tec@gmail.com>
2020-07-10 22:11:25 +0530

    added dropdown menu and card component

--- src/Components/StationComponents/PatientSearch.js
+++ src/Components/StationComponents/PatientSearch.js
@@ -1,5 +1,29 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import {CardHeader} from '@material-ui/core/';
+import CardActions from '@material-ui/core/CardActions';
+const useStyles = () => ({
+  root: {
+    maxWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 class PatientSearch extends Component {
   state = {
@@ -9,31 +33,67 @@ class PatientSearch extends Component {
       { id: 2, name: "Person 2", age: 20, gender: "F", available: true },
       { id: 3, name: "Person 3", age: 30, gender: "M", available: true },
     ],
-    selected: false,
+    station: [
+      { id: 1, available: false },
+      { id: 2, available: true },
+      { id: 3, available: true },
+    ],
+    selected: false
   };
 
   render() {
+
+    const {classes} = this.props;
+
     return (
-      <div>
+     
+     <div>
+     <Autocomplete
+      id="combo-box-demo"
+      options={this.state.people}
+      getOptionLabel={(option) => (option.available === false)? (option.id +" (busy)").toString(): (option.id).toString()}
+      getOptionDisabled={(option) => option.available === false}
+      style={{ width: 300, marginRight: 200 }}
+      renderInput={(params) => <TextField {...params} label="Search Patient ID" variant="outlined" />}
+     />
+        
         <Button
           variant="contained"
           color="primary"
-          style={{ marginTop: 20, marginRight: 20 }}
+          style={{ marginTop: 20, marginRight: 20, marginBottom:20 }}
           onClick={() => this.setState({ selected: true })}
           disabled={this.state.selected}
         >
           Get Next Person
         </Button>
+
+       
+       
         <Button
           variant="contained"
           color="primary"
-          style={{ marginTop: 20 }}
+          style={{ marginTop: 20, marginBottom:20 }}
           onClick={() => this.setState({ selected: false })}
           disabled={!this.state.selected}
         >
           Cancel
         </Button>
+
+        <Card className={classes.root}>
+         <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+        {`ID : ${this.state.people[0].id}`}
+        </Typography>
+        <Typography variant="body2" component="p">
+        {`Age : ${this.state.people[0].age}`}
+        <br/>
+        {`Gender : ${this.state.people[0].gender}`}
+        </Typography>
+      </CardContent>
+    </Card>
+
         <br />
+    
         <Button
           variant="contained"
           color="primary"
@@ -42,9 +102,11 @@ class PatientSearch extends Component {
         >
           Next
         </Button>
-      </div>
+      
+     </div>
     );
   }
 }
 
-export default PatientSearch;
+
+export default withStyles(useStyles)(PatientSearch);
