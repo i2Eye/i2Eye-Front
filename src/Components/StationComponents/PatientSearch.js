1653b2900d2f872d520dd3d57b0b37ae04ac136d
car155 <carissaying0000@gmail.com>
2020-07-12 03:11:19 +0800

    Added card logic and some input and value control. Some of it is a bit gross...

--- src/Components/StationComponents/PatientSearch.js
+++ src/Components/StationComponents/PatientSearch.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import {CardHeader} from '@material-ui/core/';
-import CardActions from '@material-ui/core/CardActions';
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import { CardHeader } from "@material-ui/core/";
+import CardActions from "@material-ui/core/CardActions";
 const useStyles = () => ({
   root: {
     maxWidth: 275,
   },
   bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
   },
   title: {
     fontSize: 14,
@@ -28,85 +28,135 @@ const useStyles = () => ({
 class PatientSearch extends Component {
   state = {
     swtich: true,
-    people: [
-      { id: 1, name: "Person 1", age: 10, gender: "M", available: false },
-      { id: 2, name: "Person 2", age: 20, gender: "F", available: true },
-      { id: 3, name: "Person 3", age: 30, gender: "M", available: true },
-    ],
+    input: "",
     station: [
       { id: 1, available: false },
       { id: 2, available: true },
       { id: 3, available: true },
     ],
-    selected: false
+    selected: null,
+  };
+
+  //Replace with function to retrieve list of people when backend team is done
+  getPeople() {
+    return [
+      { id: 1, name: "Person 1", age: 10, gender: "M", available: false },
+      { id: 2, name: "Person 2", age: 20, gender: "F", available: true },
+      { id: 3, name: "Person 3", age: 30, gender: "M", available: true },
+    ];
+  }
+
+  handleInput = (e, v, r) => {
+    this.setState({ input: v });
+  };
+
+  handleMasterSearch = (e, v, r) => {
+    this.setState({ selected: v });
+  };
+
+  //Find a way to render an alert if there is no next person in the queue
+  getNextPerson = () => {
+    const availablePeople = this.getPeople().filter(
+      (person) => person.available
+    );
+    availablePeople.length <= 0
+      ? this.setState({ selected: null })
+      : this.setState({ selected: availablePeople[0] });
+  };
+
+  getCard = (classes, person) => {
+    return (
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            {`ID : ${person.id}`}
+          </Typography>
+          <Typography variant="body2" component="p">
+            {`Name : ${person.name}`}
+            <br />
+            {`Age : ${person.age}`}
+            <br />
+            {`Gender : ${person.gender}`}
+          </Typography>
+        </CardContent>
+      </Card>
+    );
   };
 
   render() {
-
-    const {classes} = this.props;
+    const { classes } = this.props;
 
     return (
-     
-     <div>
-     <Autocomplete
-      id="combo-box-demo"
-      options={this.state.people}
-      getOptionLabel={(option) => (option.available === false)? (option.id +" (busy)").toString(): (option.id).toString()}
-      getOptionDisabled={(option) => option.available === false}
-      style={{ width: 300, marginRight: 200 }}
-      renderInput={(params) => <TextField {...params} label="Search Patient ID" variant="outlined" />}
-     />
-        
+      <div>
+        <Autocomplete
+          id="patient-master-search"
+          autoHighlight
+          options={this.getPeople()}
+          getOptionLabel={(option) =>
+            option.id.toString() +
+            " - " +
+            option.name +
+            (!option.available ? " (busy)" : "")
+          }
+          /* The search is actually a master search so that the person can put whatever patient they want next. (It's probably used in the case
+          something happends and we need to overwrite the queue manually :/// ) */
+
+          //getOptionDisabled={(option) => !option.available}
+          style={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Search Patient ID"
+              variant="outlined"
+            />
+          )}
+          onInputChange={this.handleInput}
+          inputValue={this.state.input}
+          onChange={this.handleMasterSearch}
+          //Find a better getOptionSelected function
+          getOptionSelected={(option, value) => option.id === value.id}
+        />
+
         <Button
           variant="contained"
           color="primary"
-          style={{ marginTop: 20, marginRight: 20, marginBottom:20 }}
-          onClick={() => this.setState({ selected: true })}
-          disabled={this.state.selected}
+          style={{ marginTop: 20, marginRight: 20, marginBottom: 20 }}
+          onClick={this.getNextPerson}
+          disabled={this.state.selected !== null}
         >
           Get Next Person
         </Button>
 
-       
-       
         <Button
           variant="contained"
           color="primary"
-          style={{ marginTop: 20, marginBottom:20 }}
-          onClick={() => this.setState({ selected: false })}
-          disabled={!this.state.selected}
+          style={{ marginTop: 20, marginBottom: 20 }}
+          onClick={() => this.setState({ input: "", selected: null })}
+          disabled={this.state.selected === null}
         >
           Cancel
         </Button>
 
-        <Card className={classes.root}>
-         <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-        {`ID : ${this.state.people[0].id}`}
-        </Typography>
-        <Typography variant="body2" component="p">
-        {`Age : ${this.state.people[0].age}`}
-        <br/>
-        {`Gender : ${this.state.people[0].gender}`}
-        </Typography>
-      </CardContent>
-    </Card>
-
+        {this.state.selected === null
+          ? null
+          : this.getCard(classes, this.state.selected)}
         <br />
-    
+
         <Button
           variant="contained"
           color="primary"
           style={{ marginTop: 20 }}
-          disabled={!this.state.selected}
+          disabled={this.state.selected === null}
         >
           Next
         </Button>
-      
-     </div>
+      </div>
     );
   }
 }
-
 
 export default withStyles(useStyles)(PatientSearch);
