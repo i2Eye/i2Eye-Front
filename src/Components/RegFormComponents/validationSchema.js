7928c4290d444f31f8cdc04346b87931459f745a
Liew Shi Hui <e0324820@u.nus.edu>
2020-08-15 23:55:58 +0800

    Updated validation schema for regform

--- src/Components/RegFormComponents/validationSchema.js
+++ src/Components/RegFormComponents/validationSchema.js
@@ -8,9 +8,51 @@ export const getStepValidationSchema = (step) => {
         nric: Yup.string().required("This field is required"),
         gender: Yup.string().required("This field is required"),
         birthday: Yup.date().required("This field is required"),
-        age: Yup.number().min(0).required("Postive age required"),
+        age: Yup.number().min(0).required("Age must be at least zero"),
         education: Yup.string().required("This field is required"),
         occupation: Yup.string().required("This field is required"),
-      })
+      });
+    case 1:
+      return Yup.object({
+        exercise_freq: Yup.string().required("This field is required"),
+        exercise_duration: Yup.number().min(0).required("Hours must be at least zero"),
+      });
+    case 2:
+      return Yup.object({
+        monthly_household_income: Yup.number().min(0).required("Income must be at least zero"),
+        household_count: Yup.number().min(0).required("Household count must be at least zero"),
+      });
+    case 3:
+      return Yup.object({
+        //symptoms: [],
+        cough_2_weeks: Yup.boolean().required("This field is required"),
+        cough_up_blood: Yup.boolean().required("This field is required"),
+        breathlessness: Yup.boolean().required("This field is required"),
+        weight_loss: Yup.boolean().required("This field is required"),
+        loss_of_apetite: Yup.boolean().required("This field is required"),
+        fever: Yup.boolean().required("This field is required"),
+
+        has_tubercolosis: Yup.string().required("This field is required"),
+        live_with_someone_with_tubercolosis: Yup.string().required("This field is required"),
+        other_diagnosed_with_tubercolosis_beyond_4_months: Yup.string().required("This field is required"),
+        
+        has_blood_borne_disease: Yup.boolean().required("This field is required"),
+        blood_borne_disease: Yup.string().required("This field is required"),
+        has_pre_existing_medical_conditions: Yup.boolean().required("This field is required"),
+            
+        family_has_diabetes: Yup.boolean().required("This field is required"),
+        family_diabetes_count: Yup.number().min(0).required("This field must be at least zero"),
+        
+        family_has_anemia: Yup.boolean().required("This field is required"),
+        family_anemia_count: Yup.number().min(0).required("This field must be at least zero"),
+        
+        family_has_oral_cancer: Yup.boolean().required("This field is required"),
+        family_oral_cancer_count: Yup.number().min(0).required("This field must be at least zero"),
+        
+        pre_existing_conditions: Yup.string().required("This field is required"),
+        family_pre_existing_conditions: Yup.boolean().required("This field is required"),
+      });
+    default:
+      return null;
   }
 }
