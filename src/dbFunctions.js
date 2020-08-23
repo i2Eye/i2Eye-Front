fd2b067f941d1a8c94fdbfecafc07ddafe1eaeda
car155 <carissaying0000@gmail.com>
2020-08-24 04:33:52 +0800

    Added proxy to fix CORS bug

--- src/dbFunctions.js
+++ src/dbFunctions.js
@@ -2,7 +2,7 @@
   fetch("/get_all_patients").then((response) => response.json()); */
 
 export const getPatient = async (id) =>
-  fetch("/get_data/" + id).then((repsonse) => response.json());
+  fetch("/get_data/" + id).then((response) => response.json());
 
 export const getStationAvailability = async () =>
   fetch("/get_availability").then((response) => response.json());
